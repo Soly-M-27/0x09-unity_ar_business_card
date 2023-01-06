87f1b8b49c56df1a77fc821409fcf36c73c181fc
Soly-M-27 <solymar227@icloud.com>
2023-01-05 23:24:45 -0400

    Added Buttom and AudioSource Compnents to each social media icon for AR Business Card

---
+++ Assets/Scripts/PushButtons.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushButtons : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    //Add Button Component to any GameObject you used as the button icon. 
+    //Once this script is added into each button as a coponent as well, it
+    //will communicate the methods according to the button components available
+    //methods. Add Audio source in each button and quickly acces the component
+    //in each button.
+    public void Pinterest()
+    {
+        Application.OpenURL("https://www.pinterest.com/solymar227/");
+    }
+
+    public void GitHub()
+    {
+        Application.OpenURL("https://github.com/Soly-M-27");
+    }
+
+    public void Twitter()
+    {
+        Application.OpenURL("https://twitter.com/Michi_or_Skully");
+    }
+
+    public void Linkdn()
+    {
+        Application.OpenURL("https://www.linkedin.com/in/solymar-sánchez-molina-094572220/");
+    }
+}
